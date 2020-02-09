676119f255063430e248206cc69d75005ee6bc04
”kodama” <kodamaperc@gmail.com>
2020-02-09 15:53:56 +0000

    課題再提出

--- ProfileController.php
+++ ProfileController.php
@@ -8,4 +8,23 @@ use App\Http\Controllers\Controller;
 class ProfileController extends Controller
 {
     //
+    public function add()
+    {
+        return view('admin.prpfile.create');
+    }
+    
+    public function reate()
+    {
+        return redirect('admin/prpfile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.prpfile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/prpfile/edit');
+    }
 }
